c7d2f0435517acda2be83d05c2a807afefbe3e12
Yash Gupta <ID+username@users.noreply.github.com>
2024-04-03 23:47:05 +0530

    create nested elements

--- 1-inception/1.0-helloworld/App.js
+++ 1-inception/1.0-helloworld/App.js
@@ -1,8 +1,29 @@
+/*
+    NESTED ELEMENTS:-
+    let's try to create this structure using React:
+        <div id="parent">
+            <div id="child">
+                <h1></h1>
+            </div>
+        </div>
+    ReactElement(Object) => HTML(Browser Understands)
+ */
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hi, I'm a H1 tag!"),
+    React.createElement("h1", {}, "Hi, I'm a H1 tag!"),
+  ])
+);
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "Hello World from React!"
 );
-console.log(heading);
+
+// console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(heading);
+root.render(parent);
