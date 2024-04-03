4b0567eac9a0962dc50e045f0b4d9a0370f5f01d
Yash Gupta <ID+username@users.noreply.github.com>
2024-04-04 01:02:28 +0530

    more complex nested structure

--- 1-inception/1.0-helloworld/App.js
+++ 1-inception/1.0-helloworld/App.js
@@ -2,20 +2,27 @@
     NESTED ELEMENTS:-
     let's try to create this structure using React:
         <div id="parent">
-            <div id="child">
-                <h1></h1>
+            <div id="child1">
+                <h1>Hi, I'm a H1 tag</h1>
+                <h1>Hi, I'm a H1 tag</h1>
+            </div>
+            <div id="child2">
+                <h1>Hi, I'm a H1 tag</h1>
+                <h1>Hi, I'm a H1 tag</h1>
             </div>
         </div>
     ReactElement(Object) => HTML(Browser Understands)
  */
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "Hi, I'm a H1 tag!"),
     React.createElement("h1", {}, "Hi, I'm a H1 tag!"),
-  ])
-);
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Hi, I'm a H1 tag!"),
+    React.createElement("h1", {}, "Hi, I'm a H1 tag!"),
+  ]),
+]);
 
 const heading = React.createElement(
   "h1",
