4510c63f09991bd8e161fad9ed9ec16cc7f71920
Yash Gupta <ID+username@users.noreply.github.com>
2024-04-04 20:37:02 +0530

    clean files, delete extra file

--- 1-inception/1.0-helloworld/App.js
+++ 1-inception/1.0-helloworld/App.js
@@ -1,18 +1,3 @@
-/*
-    NESTED ELEMENTS:-
-    let's try to create this structure using React:
-        <div id="parent">
-            <div id="child1">
-                <h1>Hi, I'm a H1 tag</h1>
-                <h1>Hi, I'm a H1 tag</h1>
-            </div>
-            <div id="child2">
-                <h1>Hi, I'm a H1 tag</h1>
-                <h1>Hi, I'm a H1 tag</h1>
-            </div>
-        </div>
-    ReactElement(Object) => HTML(Browser Understands)
- */
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "Hi, I'm a H1 tag!"),
@@ -30,7 +15,5 @@ const heading = React.createElement(
   "Hello World from React!"
 );
 
-// console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
 root.render(parent);
